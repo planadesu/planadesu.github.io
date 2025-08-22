e1639d9d4a77fde22d7b2c4d2e02916f3b8bcf7b
planadesu <plana.kawaii@outlook.com>
2025-08-22 09:54:57 +0800

    feat: add social link and share

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,12 @@
 import type { Props } from "astro";
+import IconMail from "@/assets/icons/IconMail.svg";
+import IconGitHub from "@/assets/icons/IconGitHub.svg";
+import IconBrandX from "@/assets/icons/IconBrandX.svg";
+import IconWhatsapp from "@/assets/icons/IconWhatsapp.svg";
+import IconFacebook from "@/assets/icons/IconFacebook.svg";
+import IconTelegram from "@/assets/icons/IconTelegram.svg";
+import IconPinterest from "@/assets/icons/IconPinterest.svg";
+import { SITE } from "./config";
 
 interface Social {
   name: string;
@@ -8,9 +16,55 @@ interface Social {
 }
 
 export const SOCIALS: Social[] = [
-
+  {
+    name: "GitHub",
+    href: "https://github.com/planadesu/planadesu.github.io",
+    linkTitle: `${SITE.author} on GitHub`,
+    icon: IconGitHub,
+  },
+  {
+    name: "Mail",
+    href: "mailto:plana.kawaii@outlook.com",
+    linkTitle: `Send an email to ${SITE.author}`,
+    icon: IconMail,
+  },
 ] as const;
 
 export const SHARE_LINKS: Social[] = [
-
+  {
+    name: "WhatsApp",
+    href: "https://wa.me/?text=",
+    linkTitle: `在WhatsApp分享`,
+    icon: IconWhatsapp,
+  },
+  {
+    name: "Facebook",
+    href: "https://www.facebook.com/sharer.php?u=",
+    linkTitle: `在Facebook分享`,
+    icon: IconFacebook,
+  },
+  {
+    name: "𝕏",
+    href: "https://x.com/intent/post?url=",
+    linkTitle: `在𝕏分享`,
+    icon: IconBrandX,
+  },
+  {
+    name: "Telegram",
+    href: "https://t.me/share/url?url=",
+    linkTitle: `在Telegram分享`,
+    icon: IconTelegram,
+  },
+  {
+    name: "Pinterest",
+    href: "https://pinterest.com/pin/create/button/?url=",
+    linkTitle: `在Pinterest分享`,
+    icon: IconPinterest,
+  },
+  {
+    name: "Mail",
+    href: "mailto:?subject=See%20this%20post&body=",
+    linkTitle: `通过邮件分享`,
+    icon: IconMail,
+  },
 ] as const;
